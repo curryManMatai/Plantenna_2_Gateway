164610900b4756a98a2d7be8926ed655be870284
curryManMatai <matairahul@gmail.com>
2021-12-14 13:46:39 +0100

    added node id removed spms library

--- local_storage/local_database.py
+++ local_storage/local_database.py
@@ -1,6 +1,7 @@
 import mysql.connector
-import spms_cloud_control
+# import spms_cloud_control
 from time import sleep
+
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
@@ -8,10 +9,10 @@ mydb = mysql.connector.connect(
     database = "plantenna"
     )
 mycursor = mydb.cursor()
-def insert_data(temp, humidity, batV, airflow, pressure):
-    sql = "INSERT INTO Sensor_data(temperature, humidity, battery_voltage, airflow, pressure) " \
-            "VALUES(%s,%s,%s,%s,%s)"
-    val = (temp, humidity, batV, airflow, pressure)
+def insert_data(temp, humidity, batV, airflow, pressure,nodeID):
+    sql = "INSERT INTO Sensor_data(temperature, humidity, battery_voltage, airflow, pressure, nodeID) " \
+            "VALUES(%s,%s,%s,%s,%s,%s)"
+    val = (temp, humidity, batV, airflow, pressure, nodeID)
     mycursor.execute(sql, val)
     mydb.commit()
     print(mycursor.rowcount, "record inserted.")
@@ -19,27 +20,27 @@ def insert_data(temp, humidity, batV, airflow, pressure):
 def return_data():
     mycursor.execute ("SELECT * FROM Sensor_data")
     myresult = mycursor.fetchall()
-    return(myresult)
-##        for result in myresult:
-##            date = str(result[6].date())
-##            time = str(result[6].time())
-##            temperature = float(result[1])/100
-##            humidity = float(result[2])/100
-##            batV = float(result[3])*20
-##            airflow = float(result[4])
-##            pressure = float(result[5])
-##            print(result)
-##            spms_mqtt_client = spms_cloud_control.spms_mqtt_init()
-##            while(1):
-##                if (spms_mqtt_client != False):
-##                    try:
-##                        spms_cloud_control.spms_mqtt_send_data(spms_mqtt_client, temperature, humidity, pressure, batV, airflow, date, time)
-##                        sleep(1)
-##                    except:
-##                        break
-##                else:
-##                    break
-                    
+    return myresult
+    # for result in myresult:
+    #     date = str(result[6].date())
+    #     time = str(result[6].time())
+    #     temperature = float(result[1])/100
+    #     humidity = float(result[2])/100
+    #     batV = float(result[3])*20
+    #     airflow = float(result[4])
+    #     pressure = float(result[5])
+    #     print(result)
+    #     spms_mqtt_client = spms_cloud_control.spms_mqtt_init()
+    #     while(1):
+    #         if (spms_mqtt_client != False):
+    #             try:
+    #                 spms_cloud_control.spms_mqtt_send_data(spms_mqtt_client, temperature, humidity, pressure, batV, airflow, date, time)
+    #                 sleep(1)
+    #             except:
+    #                 break
+    #         else:
+    #             break
+                
                 
 def remove_data(num):
     sql = "DELETE FROM Sensor_data WHERE number = " + str(num)
@@ -49,10 +50,10 @@ def remove_data(num):
 
 
 def main():
-    print('a')
-##    for i in range(10):
-##        insert_data('23','90','10','12','3')
-##    return_data()
+    
+    for i in range(12):
+        insert_data('23','90','10','12','3','5')
+    print(return_data())
 ##    remove_data(1120)
 if __name__ == '__main__':
     main()
