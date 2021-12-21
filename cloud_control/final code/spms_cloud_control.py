e261e238f695987b71a9441e22c7de3b0b2b1af7
curryManMatai <matairahul@gmail.com>
2021-12-21 15:57:12 +0100

    Update spms_cloud_control.py
    gets data from dataabase automatically

--- cloud_control/final code/spms_cloud_control.py
+++ cloud_control/final code/spms_cloud_control.py
@@ -9,26 +9,41 @@
 ###############################################################
 
 # import
-import smbus2
-import bme280
+import local_database
+# import mysql.connector
+# import smbus2
+# import bme280
 import paho.mqtt.client as mqtt
 import json
 from time import sleep
 
 # global variables
-THINGSBOARD_HOST = 'plantenna.nl'
-ACCESS_TOKEN = 'h2WcsjGlrQrbhsQcnzGU'
+THINGSBOARD_HOST = '52.157.215.225'
+ACCESS_TOKEN = '6IVenDvEvKlI5QJ3hhn8'
+
 
 INTERVAL = 2
 
-sensor_data = {'temperature': 0, 'humidity': 0, 'pressure': 0, 'batteryVoltage': 0, 'airflow': 0, 'date' : 0 , 'time': 0}
+sensor_data = local_database.return_lat_data()
+sensor_data = {k.encode("utf-8"): str(v) for k,v in sensor_data.iteritems()}
+print('init %s', sensor_data)
+# print(sensor_data["temperature"])
 
 # functions
+def get_data():
+    data = local_database.return_lat_data()
+    data = {k.encode("utf-8"): str(v) for k,v in data.iteritems()}
+    print('getdata')
+    print(data)
+
+    return data
+
 def spms_mqtt_init():
     client = mqtt.Client()
     
     # Set access token
     client.username_pw_set(ACCESS_TOKEN)
+
 
     # Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
     try:
@@ -40,20 +55,20 @@ def spms_mqtt_init():
     
     return client
 
-def spms_mqtt_send_data(client, temperature , humidity , pressure, batV, airflow, date, time):
+def spms_mqtt_send_data(client, temperature , humidity , pressure, batV, airflow, date):
     sensor_data['temperature'] = temperature
     sensor_data['humidity'] = humidity
     sensor_data['pressure'] = pressure
-    sensor_data['batteryVoltage'] = batV
+    sensor_data['battery_voltage'] = batV
     sensor_data['airflow'] = airflow
     sensor_data['date'] = date
-    sensor_data['time'] = time
+ 
     
-    while(1):
-        try:
-            client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
-        finally:
-            break
+    try:
+        client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+        return 0
+    except:
+        return 1
         
 def spms_mqtt_stop(client):
     client.loop_stop()
@@ -66,8 +81,14 @@ def main():
     
     try:
         while True:
-            spms_mqtt_send_data(spms_mqtt_client, 0, 0, 0, 0, 0, 0, 0)
+            sensor_data = get_data()
+            err = spms_mqtt_send_data(spms_mqtt_client, sensor_data['temperature'], sensor_data['humidity'], sensor_data['pressure'], sensor_data['battery_voltage'], sensor_data['airflow'],sensor_data['date'])
+            print(sensor_data['temperature'])
+            print(err)
             sleep(2)
+
+            
+
     finally:
         spms_mqtt_stop(spms_mqtt_client)
     
